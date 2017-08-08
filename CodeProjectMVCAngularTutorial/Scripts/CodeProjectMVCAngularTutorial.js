6b86a297b8523f1b6d940b46ed759d6fe6f206e2
VirtualProtocol <adam.pankow@outlook.com>
2017-08-08 06:57:43 -0500

    MVC Angular Tutorial Progress: Part 2 https://www.codeproject.com/Articles/806500/Getting-started-with-AngularJS-and-ASP-NET-MVC-P

---
+++ CodeProjectMVCAngularTutorial/Scripts/CodeProjectMVCAngularTutorial.js
@@ -0,0 +1,27 @@
+﻿var CodeProjectMVCAngularTutorial = angular.module('CodeProjectMVCAngularTutorial', ['ngRoute']);
+
+CodeProjectMVCAngularTutorial.controller('LandingPageController', LandingPageController);
+CodeProjectMVCAngularTutorial.controller('LoginController', LoginController);
+CodeProjectMVCAngularTutorial.controller('RegisterController', RegisterController);
+
+CodeProjectMVCAngularTutorial.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+CodeProjectMVCAngularTutorial.factory('LoginFactory', LoginFactory);
+CodeProjectMVCAngularTutorial.factory('RegistrationFactory', RegistrationFactory);
+
+var configFunction = function ($routeProvider, $httpProvider, $locationProvider)
+{
+
+    $locationProvider.hashPrefix('!').html5Mode(true);
+
+    $routeProvider.
+        when('/routeOne', { templateUrl: 'routesDemo/one' })
+        .when('/routeTwo/:donuts', { templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }})
+        .when('/routeThree', { templateUrl: 'routesDemo/three' })
+        .when('/login', { templateUrl: '/Account/Login', controller: LoginController });
+        .when('/register', { templateUrl: '/Account/Register', controller: RegisterController })
+
+    $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+}
+configFunction.$inject = ['$routeProvider', '$httpProvider', '$locationProvider'];
+
+CodeProjectMVCAngularTutorial.config(configFunction);
